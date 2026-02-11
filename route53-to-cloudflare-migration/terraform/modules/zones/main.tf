a55926441ef324f35e13293005148df6a7057eeb
tdlmatias <4129083+tdlmatias@users.noreply.github.com>
2026-02-11 16:44:52 +0000

    Removing Merge Comment

--- route53-to-cloudflare-migration/terraform/modules/zones/main.tf
+++ route53-to-cloudflare-migration/terraform/modules/zones/main.tf
@@ -1,22 +1,4 @@
-<<<<<<< HEAD:route53-to-cloudflare-migration/terraform/modules/zone/main.tf
-resource "cloudflare_zone" "zone" {
-  account_id = var.account_id
-  zone       = var.zone_name
-}
 
-resource "cloudflare_record" "dns_records" {
-  for_each = {
-    for idx, record in var.records :
-    "${record.name}-${record.type}" => record
-  }
-
-  zone_id = cloudflare_zone.zone.id
-  name    = each.value.name
-  type    = each.value.type
-  value   = each.value.content
-  ttl     = each.value.ttl
-  proxied = false
-=======
 resource  "cloudflare_zone" "zone" {
     account_id  = var.account_id  # you may need to pass this in
     zone        = var.zone_name
@@ -34,5 +16,4 @@ resource "cloudflare_record" "dns_records" {
     value    = each.value.content
     ttl      = each.value.ttl
     proxied  = false
->>>>>>> origin/feature_deployment_v02:route53-to-cloudflare-migration/terraform/modules/zones/main.tf
 }
