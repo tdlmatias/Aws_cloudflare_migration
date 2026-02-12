b6b59f00a17adc1a51765bb26d14635aec3dc7ae
Tchize Matias <4129083+tdlmatias@users.noreply.github.com>
2026-02-12 11:06:24 +0000

    Update Cloudflare version

--- route53-to-cloudflare-migration/terraform/modules/zones/variables.tf
+++ route53-to-cloudflare-migration/terraform/modules/zones/variables.tf
@@ -17,3 +17,11 @@ variable "records" {
     ttl     = number
   }))
 }
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token (optional if CLOUDFLARE_API_TOKEN env var is set)."
+  type        = string
+  sensitive   = true
+  default     = null
+  nullable    = true
+}
